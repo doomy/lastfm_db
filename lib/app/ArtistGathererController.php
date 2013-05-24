9626ef91a9b4a792220b35b581d5eb56c0fa6ce4
doomy <vladimir.bartek@gmail.com>
2013-05-24 11:55:03 +0200

    Get contents of the start page

--- lib/app/ArtistGathererController.php
+++ lib/app/ArtistGathererController.php
@@ -1,14 +1,16 @@
 <?php
 class ArtistGathererController extends BasePackage {
-// version 3
+// version 4
     public function __construct($env) {
         $this->env = $env;
-        $this->include_packages(array('log'));
+        $this->include_packages(array('log', 'file'));
         $this->log = new Log('artist_gatherer', $this->env, array('stdout', 'filesystem'));
     }
 
     public function run() {
-        $this->log->log($this->env->ENV_VARS["gatherer_start_page"]);
+        $start_page = new File($this->env);
+        $start_page->set_name($this->env->ENV_VARS["gatherer_start_page"]);
+        $this->log->log($start_page->get_contents());
     }
 }
 ?>
