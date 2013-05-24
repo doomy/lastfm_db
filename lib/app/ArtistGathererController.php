8f05425ca2a9ecbf59c73af480e2752dce904922
doomy <vladimir.bartek@gmail.com>
2013-05-24 11:32:17 +0200

    Simple test for the Artist Gatherer Controller

--- lib/app/ArtistGathererController.php
+++ lib/app/ArtistGathererController.php
@@ -1,9 +1,12 @@
 <?php
 class ArtistGathererController extends BasePackage {
 // version 3
+    public function __construct($env) {
+        $this->env = $env;
+    }
 
     public function run() {
-        echo 'Artist Gatherer Controller runs';
+        echo $this->env->ENV_VARS["gatherer_start_page"];
     }
 }
 ?>
