fe6f4545d010736aba8ef995db888e41fb4e3101
doomy <vladimir.bartek@gmail.com>
2013-05-24 12:29:45 +0200

    Fetch music links from the page

--- lib/app/ArtistGathererController.php
+++ lib/app/ArtistGathererController.php
@@ -10,7 +10,16 @@ class ArtistGathererController extends BasePackage {
     public function run() {
         $start_page = new File($this->env);
         $start_page->set_name($this->env->ENV_VARS["gatherer_start_page"]);
-        $this->log->log($start_page->get_contents());
+        $content = $start_page->get_contents();
+        $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+        if(preg_match_all("/$regexp/siU", $content, $matches)) {
+            foreach ($matches[2] as $match) {
+                if(strpos($match, 'music/') > 0)
+                    $this->log->log($match);
+                // $matches[2] = array of link addresses
+                //$matches[3] = array of link text - including HTML code
+            }
+        }
     }
 }
 ?>
