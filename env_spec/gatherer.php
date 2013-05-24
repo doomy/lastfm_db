edb5f3300e88badc0e6646e325a0213c73d53f28
doomy <vladimir.bartek@gmail.com>
2013-05-24 11:10:49 +0200

    Add start page to gatherer env_spec

---
+++ env_spec/gatherer.php
@@ -0,0 +1,3 @@
+<?php
+    $ENV_VARS["gatherer_start_page"] = 'http://www.last.fm/user/_ch4ng3d/charts?rangetype=overall&subtype=artists';
+?>
--- lib/app/ArtistGathererController.php
+++ lib/app/ArtistGathererController.php
@@ -1,5 +1,5 @@
 <?php
 class ArtistGathererController {
-
+// version 2
 }
 ?>
