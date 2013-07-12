1d80b0e0776fb9d8596f9bfee7743bd7b573569b
doomy <vladimir.bartek@gmail.com>
2013-07-12 11:57:43 +0200

    Actual version

--- env_spec/gatherer.php
+++ env_spec/gatherer.php
@@ -1,3 +1,3 @@
 <?php
-    $ENV_VARS["gatherer_start_page"] = 'http://www.last.fm/user/trondskap/charts?rangetype=3month&subtype=artists';
+    $ENV_VARS["gatherer_start_page"] = 'http://www.last.fm/user/NespaLa/charts?rangetype=overall&subtype=artists';
 ?>
