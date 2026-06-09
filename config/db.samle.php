0664f5885e76e10d6aa2eaf449fdfc9fc7786efc
Vladimir Bartek <vladimir.bartek@gmail.com>
2026-06-09 14:58:41 +0200

    configuration uplift

--- config/db.samle.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$CONFIG['DB_HOST'] = '127.0.0.1';
-$CONFIG['DB_PORT'] = 3306;
-$CONFIG['DB_NAME'] = 'dummy_lastfm_db';
-$CONFIG['DB_USER'] = 'dummy_user';
-$CONFIG['DB_PASS'] = 'dummy_password';
-$CONFIG['DB_CREATE'] = 0;
